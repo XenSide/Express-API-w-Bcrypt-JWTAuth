4a7cc834fdaaca5f6a86b407fe725e7ba799317c
Claudio Dalfino <xenside@gmail.com>
2024-03-20 11:58:37 +0100

    cleaned up loginHandler code
    removed unecessary Promise

--- controller.js
+++ controller.js
@@ -3,27 +3,21 @@ const { compare } = require("bcrypt");
 
 const jwt = require("jsonwebtoken");
 
-// eslint-disable-next-line no-undef
 function generateJWT(username, secret = process.env.JWT_SECRET) {
   return jwt.sign({ username: username }, secret, { expiresIn: "1h" });
 }
 
 async function loginHandler(username, password) {
-  let user;
-  user = await getUser(username);
-  if (user) {
-    return new Promise((resolve, reject) => {
-      compare(password, user.password, (err, res) => {
-        if (!user || !res) {
-          reject(new Error("invalid username or password"));
-        } else {
-          resolve(user);
-        }
-      });
-    });
-  } else {
-    throw new Error("invalid username or password");
+  const user = await getUser(username);
+  if (!user) {
+    throw new Error("Invalid credentials");
   }
+  const match = await compare(password, user.password);
+  if (!match) {
+    throw new Error("Invalid credentials");
+  }
+
+  return user;
 }
 
 module.exports = { generateJWT, loginHandler };
